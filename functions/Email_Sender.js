87f553e9745e76abf8ace3bb39b80ff9dfca65bf
Martin <acostajoma9@gmail..com>
2024-06-29 15:15:11 -0600

    Changes related to values

--- functions/Email_Sender.js
+++ functions/Email_Sender.js
@@ -6,7 +6,7 @@ exports = async function() {
   const mongodb = context.services.get("mongodb-atlas");
 
   // Acceder a los valores correctamente
-  const databaseName = context.values.get('DATABASE_NAME');
+  const databaseName = context.environments.values.databaseName
   const db = mongodb.db(databaseName); 
   const collection = db.collection("ContactData");
 
@@ -22,8 +22,10 @@ exports = async function() {
   }
 
   // Acceder a los secretos correctamente
-  const emailUser = context.secrets.get('EMAIL_USER');
-  const mailerSendApiKey = context.secrets.get('MAILER_SEND_API_KEY');
+  const environment = context.environment.tag
+  const emailUser = context.environments.values.emailUser
+  const mailerSendApiKey = context.values.get(`mailerSendApiKey_${environment}`);
+  
 
   const mailersend = new MailerSend({
     api_key: mailerSendApiKey
